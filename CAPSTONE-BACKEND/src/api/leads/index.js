c9d73fbb98a663af082b9ea30bb28116fbab6384
m.setyaadjie <135620776+setya20040304@users.noreply.github.com>
2025-12-14 11:10:42 +0700

    Delete CAPSTONE-BACKEND directory

--- CAPSTONE-BACKEND/src/api/leads/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('./routes');
-
-module.exports = {
-  name: 'api-leads',
-  version: '1.0.0',
-  register: async (server, options) => {
-    server.route(routes);
-  },
-};
--- CAPSTONE-BACKEND/src/api/users/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('./routes');
-
-module.exports = {
-  name: 'api-users',
-  version: '1.0.0',
-  register: async (server, options) => {
-    server.route(routes);
-  },
-};
