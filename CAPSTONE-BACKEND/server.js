136d90a37ec91600bd408dfa554a332943d092f3
m.setyaadjie <135620776+setya20040304@users.noreply.github.com>
2025-12-14 13:04:38 +0700

    Update server configuration to use environment variables

--- CAPSTONE-BACKEND/server.js
+++ CAPSTONE-BACKEND/server.js
@@ -4,8 +4,8 @@ const Hapi = require('@hapi/hapi');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: process.env.PORT || 5000,
+    host: process.env.HOST || '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'], 
@@ -32,5 +32,6 @@ process.on('unhandledRejection', (err) => {
   console.log(err);
   process.exit(1);
 });
+
 
 init();
